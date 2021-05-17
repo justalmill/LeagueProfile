a7819c94cbbfbd899335c8736625141092b61742
justalmill <50275799+justalmill@users.noreply.github.com>
2021-05-16 20:22:55 -0400

    Match History Changes

--- server/server.js
+++ server/server.js
@@ -48,14 +48,38 @@ for (c in JsonData.data) {
 	champs.set(JsonData.data[c].key, JsonData.data[c].name);
 }
 
-var api_key = 'RGAPI-8202251f-07d0-4acf-b6af-f805975f46da';
+var summonerSpells = new Map();
+summonerSpells.set(1, "Boost");
+summonerSpells.set(3, "Exhaust");
+summonerSpells.set(4, "Flash");
+summonerSpells.set(5, "Teleport");
+summonerSpells.set(6, "Haste");
+summonerSpells.set(7, "Heal");
+summonerSpells.set(11, "Smite");
+summonerSpells.set(12, "Teleport");
+summonerSpells.set(13, "Mana");
+summonerSpells.set(14, "Dot");
+summonerSpells.set(21, "Barrier");
+summonerSpells.set(32, "Snowball");
+
+var api_key = 'RGAPI-9eae8f2d-67de-4f01-9ff1-9288953b3ab2';
 
 app.route('/api/summoner/:name').get((req, res) => {
 	var getreq = https.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ req.params.name + "?api_key=" + api_key, function(result) {
 		result.on("data", function(chunk) {
 			res.header("Access-Control-Allow-Origin", "*");
 			res.send(chunk);
-			console.log("BODY: " + chunk);
+			//console.log("BODY: " + chunk);
+		});
+	});
+})
+
+app.route('/api/rank/:id').get((req, res) => {
+	var getreq = https.get("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ req.params.id + "?api_key=" + api_key, function(result) {
+		result.on("data", function(chunk) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.send(chunk);
+			//console.log("BODY: " + chunk);
 		});
 	});
 })
@@ -69,13 +93,60 @@ app.route('/api/mastery/:id').get((req, res) => {
 		
 		result.on("end", ()=> {
 			res.header("Access-Control-Allow-Origin", "*");
+			//console.log(body);
+			//TODO check that champ name key exists, can break on champ release otherwise
 			var JsonMastery = JSON.parse(body)
 			for ( c in JsonMastery ) {
 				JsonMastery[c].champName = JsonMastery[c].championId ? champs.get(JsonMastery[c].championId.toString()) : "";
-				JsonMastery[c].champIcon = JsonMastery[c].championId? champs.get(JsonMastery[c].championId.toString()).replace(/\s/g, "") : "";
+				JsonMastery[c].champIcon = JsonMastery[c].championId ? champs.get(JsonMastery[c].championId.toString()).replace(/\s/g, "") : "";
 			}
 			res.send(JsonMastery);	
 		})
 	});
 })
 
+app.route('/api/matchIds/:id/:startidx/:count').get((req, res) => {
+	var getMatchIds = https.get("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + req.params.id + "/ids?start=" + req.params.startidx + "&count=" + req.params.count + "&api_key=" + api_key, function(result) {
+		var body = "";
+		result.on("data", function (chunk) {
+			body += chunk;
+		});
+		result.on("end", ()=> {
+			//console.log(body);
+			res.header("Access-Control-Allow-Origin", "*");
+			res.send(body);
+		})
+	});	
+})
+
+app.route('/api/matchData/:id').get((req, res) => {
+	var getMatchData = https.get("https://americas.api.riotgames.com/lol/match/v5/matches/" + req.params.id + "?api_key=" + api_key, function(result) {
+		var body = "";
+		var mbody = [];
+		var responses = [];
+		result.on("data", function (chunk) {
+			body += chunk;
+		});
+		result.on("end", ()=> {
+			jsonBody = JSON.parse(body);
+			if (jsonBody.info) {
+				for (p in jsonBody.info.participants) {
+					if (summonerSpells.has(jsonBody.info.participants[p].summoner1Id)) {
+						jsonBody.info.participants[p].summoner1Name = summonerSpells.get(jsonBody.info.participants[p].summoner1Id); 
+					}
+					else {
+						jsonBody.info.participants[p].summoner1Name = "Unknown"
+					}
+					if (summonerSpells.has(jsonBody.info.participants[p].summoner2Id)) {
+						jsonBody.info.participants[p].summoner2Name = summonerSpells.get(jsonBody.info.participants[p].summoner2Id); 
+					}
+					else {
+						jsonBody.info.participants[p].summoner2Name = "Unknown"
+					}
+				}
+			}
+			res.header("Access-Control-Allow-Origin", "*");
+			res.send(jsonBody);
+		})
+	});	
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,70 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   constructor(private http: HttpClient, private _ngZone: NgZone, private changeDetector: ChangeDetectorRef) {}
+  
+  //TODO move most of this into a new service
   title = 'LeagueProfile';
   summoner!: { id: string; accountId: string; puuid: string; name: string; profileIconId: number; revisionDate: number; summonerLevel: number; };
+  rank?: {freshBlood: boolean; hotStreak: boolean; inactive: boolean; leagueId: string; leaguePoints: number; losses: number; queueType: string; rank: string; summonerId: string; summonerName: string; tier: string; veteran: boolean; wins: number};
   champMastery!: Array<{championId?: number; championLevel?: number; champName?: string; champIcon?: string; championPoints?: number; lastPlayTime?: number; championPointsSinceLastLevel?: number; championPointsUntilNextLevel?: number; chestGranted?: boolean; tokensEarned?: number; summonerId?: string;}>;
+  matchHistory: Array<{info: {gameCreation: number; gameDuration: number; gameId: number; gameMode: string; gameStartTimestamp: number; mapId: number; queueId: number; participants: Array<{ assists: number; baronKills: number; championName: string; deaths: number; kills: number; puuid: number; summoner1Name: string; summoner2Name: string; }>}; player: { assists: number; baronKills: number; championName: string; deaths: number; kills: number; puuid: number;  summoner1Name: string; summoner2Name: string;};}> = [];
   
   GetSummoner(value: string): void 
   {
-    this.GetFromApi("summoner", value).subscribe((result: any) => {
+    // Switching summoner profiles wipes matchhistory and rank data
+    this.matchHistory = [];
+    this.rank = undefined;
+    this.GetFromApi("summoner", [value]).subscribe((result: any) => {
       this.summoner = result; 
-      console.log(result);
+      //console.log(result);
+      this.GetRank(this.summoner.id);
       this.GetMastery(this.summoner.id);
+      this.GetMatchHistory(this.summoner.puuid, 0, 10);
     });
+  }
+
+  GetRank(id: string) {
+    this.GetFromApi("rank", [this.summoner.id]).subscribe((result: any) => {
+      this.rank = result[0];
+      console.log(this.rank);
+    })
   }
 
   GetMastery(id: string): void 
   {
-    this.GetFromApi("mastery", id).subscribe((result: any) => {this.champMastery = result; console.log(result);});
+    let params = [id]
+    this.GetFromApi("mastery", params).subscribe((result: any) => {this.champMastery = result; console.log(result);});
   }
 
-  GetFromApi (dest: string, value: string): Observable<any> {
-    let riotUrl = "http://localhost:8000/api/" + dest + "/" + value;
-    return this.http.get(riotUrl).pipe(map(result => result !== null ? result : ""), tap(idString => console.log(idString), debounceTime(300)));
+  GetMatchHistory(id: string, start: number, end: number): void 
+  {
+    let params = [id, start, end] 
+    this.GetFromApi("matchIds", params).subscribe((ids: any) => {
+      for (let i in ids) {
+        this.GetFromApi("matchData", [ids[i]]).subscribe((result: any) => {
+          if (result.info) {
+            result.player = result.info.participants.find((element: any) => element.puuid === this.summoner.puuid);
+            this.matchHistory.push(result);
+            console.log(result);
+            //sort to chronological order
+            if (Number(i) == ids.length-1) {
+              this.matchHistory.sort((first, second) => {return second.info.gameStartTimestamp - first.info.gameStartTimestamp});
+            }
+          }
+        },
+        (error: any) => {console.log(error);}
+        );
+      }
+    });
+    
+  }
+
+  GetFromApi (dest: string, value: any[]): Observable<any> {
+    let apiUrl = "http://localhost:8000/api/" + dest ;
+    value.forEach(e => {
+      apiUrl +=  "/" + e;
+    });
+    return this.http.get(apiUrl).pipe(map(result => result !== null ? result : ""));//, tap(idString => console.log(idString)));
   }
 
 
