aede59c27cdef68252cc659d5ca3bcb8e5f79c9f
justalmill <50275799+justalmill@users.noreply.github.com>
2021-06-06 20:45:12 -0400

    match history formatting and win/loss coloring

--- src/app/shared/match-history/match-history.component.ts
+++ src/app/shared/match-history/match-history.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MatchHistoryComponent implements OnInit {
   @Input()
-  matchHistory?: Array<{info: {gameCreation: number; gameDuration: number; gameId: number; gameMode: string; gameStartTimestamp: number; mapId: number; queueId: number; participants: Array<{ assists: number; baronKills: number; championName: string; deaths: number; kills: number; puuid: number; summoner1Name: string; summoner2Name: string; }>}; player: { assists: number; baronKills: number; championName: string; deaths: number; kills: number; puuid: number;  summoner1Name: string; summoner2Name: string;};}>;
+  matchHistory?: Array<{info: {gameCreation: number; gameDuration: number; gameId: number; gameMode: string; gameStartTimestamp: number; mapId: number; queueId: number; participants: Array<{ assists: number; baronKills: number; championName: string; deaths: number; kills: number; puuid: number; summoner1Name: string; summoner2Name: string; }>}; player: { assists: number; baronKills: number; championName: string; deaths: number; kills: number; puuid: number;  summoner1Name: string; summoner2Name: string; win: boolean; teamEarlySurrendered: boolean};}>;
  
 
   constructor() { }
